512fef6f60131f88b8002e8a02e3644bdddaa566
john <johnjohn@johns-MacBook-Pro-3.local>
2025-12-08 12:12:01 -0800

    Add layout.tsx to force dynamic rendering on /create page

---
+++ src/app/create/layout.tsx
@@ -0,0 +1,11 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
+export default function CreateLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
+
